76ae3c3747bc0b3f43929ebc22637ffad90a6821
Michael Kumm <michael@mkumm.com>
2018-08-19 09:23:13 -0400

    updated 1-3
    Forgot about case where a, b, or c could be equal

--- src/section1-1/exercise1-3.rkt
+++ src/section1-1/exercise1-3.rkt
@@ -2,9 +2,9 @@
 
 (define (sum-of-2-biggest-squares a b c)
   (cond
-    ((and (< a b) (< a c)) (sum-of-squares b c))
-    ((and (< b c) (< b a)) (sum-of-squares a c))
-    ((and (< c a) (< c b)) (sum-of-squares a b)))
+    ((and (<= a b) (<= a c)) (sum-of-squares b c))
+    ((and (<= b c) (<= b a)) (sum-of-squares a c))
+    ((and (<= c a) (<= c b)) (sum-of-squares a b)))
   )
 
 (define (sum-of-squares a b)
