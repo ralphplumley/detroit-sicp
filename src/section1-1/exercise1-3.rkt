0d22c7fd83ccf2571ffe8bc99f53b132a08494a8
Michael Kumm <michael@mkumm.com>
2018-08-19 00:55:51 -0400

    first 3 exercises from ssection 1
    Exercise 1-3 feels dirty - but - I didn’t know how to return a list that could be used in a function. AND I did not know how to return a defaut in a cond. AND I avoid nested ifs like the plague - which means I am pretty much in trouble for the rest of the book :)

---
+++ src/section1-1/exercise1-3.rkt
@@ -0,0 +1,14 @@
+#lang sicp
+
+(define (sum-of-2-biggest-squares a b c)
+  (cond
+    ((and (< a b) (< a c)) (sum-of-squares b c))
+    ((and (< b c) (< b a)) (sum-of-squares a c))
+    ((and (< c a) (< c b)) (sum-of-squares a b)))
+  )
+
+(define (sum-of-squares a b)
+  (+ (square a) (square b)))
+
+(define (square x)
+  (* x x))
