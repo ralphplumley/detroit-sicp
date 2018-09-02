e5ae883001295c8d9e403eacf4503f81bed0d5ea
Ralph Plumley <ralphplumley@gmail.com>
2018-09-01 21:27:29 -0400

    attempt up to 1.4

---
+++ exercise1-3.rkt
@@ -0,0 +1,14 @@
+#lang sicp
+
+(define (sum-2-biggest-squares a b c)
+  (cond
+    ; if a is smallest
+    ((and (<= a b) (<= a c)) (+ (* b b) (* c c)))
+    ; if b is smallest
+    ((and (<= b a) (<= b c)) (+ (* a a) (* c c)))
+    ; if c is smallest
+    ((and (<= c b) (<= c a)) (+ (* b b) (* a a)))
+  )
+)
+
+; (sum-2-biggest-squares 2 3 4) >> 25
