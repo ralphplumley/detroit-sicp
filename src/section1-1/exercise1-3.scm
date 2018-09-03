4623a9319ada8069957e4c03711748772b38f0d4
Michael Kumm <michael@mkumm.com>
2018-09-03 09:46:26 +0200

    80 col max

--- src/section1-1/exercise1-3.scm
+++ src/section1-1/exercise1-3.scm
@@ -2,9 +2,11 @@
 
 ; Exercise 1.3
 ;
-; Define a procedure that takes three numbers as arguments and returns the sum of the squares of the two larger numbers
+; Define a procedure that takes three numbers as arguments and returns the sum 
+; of the squares of the two larger numbers
 
-; I wish I knew how to separate out a procedure to return the 2 largest numbers from a list of 3
+; I wish I knew how to separate out a procedure to return the 2 largest 
+; numbers from a list of 3
 
 (define (sum-of-2-biggest-squares a b c)
   (cond
--- src/section1-1/exercise1-4.scm
+++ src/section1-1/exercise1-4.scm
@@ -2,7 +2,9 @@
 
 ; Exercise 1.4
 ;
-; Observe that our model of evaluation allows for the combinations whose operators are compound expressions. Use this observation to describe the behavior of the following procedure:
+; Observe that our model of evaluation allows for the combinations whose
+; operators are compound expressions. Use this observation to describe the 
+; behavior of the following procedure:
 
 (define (a-plus-abs-b a b)
   ((if (> b 0) + -) a b))
@@ -11,6 +13,9 @@
 ;   if b is greater than 0, add a.
 ;   otherwise, add b.
 
-; The if statement in this procedure will return a different operator depending on the value of b.  
-; If b is positive, it is the (+) operator, if it is negative, it is the (-) operator.  
-; The procedure will thus return (+ a b) if b is positive, and (- a b) if b is negative.
+; The if statement in this procedure will return a different operator 
+; depending on the value of b.  
+; If b is positive, it is the (+) operator, if it is negative, it is the (-) 
+; operator.  
+; The procedure will thus return (+ a b) if b is positive, and (- a b) if b 
+; is negative.
