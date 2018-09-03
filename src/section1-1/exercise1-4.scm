a4ddebfa27449bf5f48fa24bf7d44302f83a5e04
Michael Kumm <michael@mkumm.com>
2018-09-03 08:35:33 +0200

    changed file type, added 1.4 answers

---
+++ src/section1-1/exercise1-4.scm
@@ -0,0 +1,16 @@
+#lang sicp
+
+; Exercise 1.4
+;
+; Observe that our model of evaluation allows for the combinations whose operators are compound expressions. Use this observation to describe the behavior of the following procedure:
+
+(define (a-plus-abs-b a b)
+  ((if (> b 0) + -) a b))
+
+; To add a and the abs value of b,
+;   if b is greater than 0, add a.
+;   otherwise, add b.
+
+; The if statement in this procedure will return a different operator depending on the value of b.  
+; If b is positive, it is the (+) operator, if it is negative, it is the (-) operator.  
+; The procedure will thus return (+ a b) if b is positive, and (- a b) if b is negative.
